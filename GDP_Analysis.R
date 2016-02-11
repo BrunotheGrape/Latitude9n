be07e0b715168801b4ae8d5c277834a7bba208d5
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-02-10 21:41:53 -0600

    with Chile plot

--- GDP_Analysis.R
+++ GDP_Analysis.R
@@ -3,3 +3,17 @@
 library(dplyr); library(ggplot2)
 Data <- read.csv("World_Bank_GDP.csv",header = TRUE)
 View(Data)
+
+
+GData <- Data %>% filter(Country.Code %in% c("CHN","CHL","PER")) %>% select(Country.Code,X2004:X2014)
+
+TData <- t(GData)
+colnames(TData) <- TData[1,]
+TData <- TData[-1,]
+dfData <- as.data.frame(TData)
+
+p <- ggplot(dfData, aes(x=CHN,y=CHL)) 
+p <- p + geom_point(colour = "darkblue", size = 4)
+p <- p + xlab("China") + ylab("Chile")
+        
+p
